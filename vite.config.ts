f945f2678126d3f72f3c64533810c4be02d6adce
Udin150104 <udin150104@gmail.com>
2026-03-04 01:54:30 +0700

    build dan change bas vire config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "assesment-kledo-fe-maret-2026",
+  base: "/assesment-kledo-fe-maret-2026/",
   plugins: [
     react(),
     tailwindcss()
